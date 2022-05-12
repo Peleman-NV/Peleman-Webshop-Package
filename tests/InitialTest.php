5f200f021359633b1c84c9c8d79d14b16f22aefa
KaiH <kai@helsen.info>
2022-05-12 12:05:48 +0200

    unit cleanup and template improvement

--- tests/InitialTest.php
+++ tests/InitialTest.php
@@ -12,4 +12,27 @@ class InitialTest extends TestCase
     {
         $this->assertTrue(true, "most basic test failed., true is not asserted as true.");
     }
+
+    /**
+     * Undocumented function
+     * @dataProvider booleanProvider
+     * @param boolean $a
+     * @param boolean $b
+     * @param boolean $expected
+     * @return void
+     */
+    public function testDataProvision(bool $a, bool $b, bool $expected): void
+    {
+        $this->assertEquals($expected, ($a === $b), "error with data: {$a} equals {$b} should equivalate to {$expected}");
+    }
+
+    public function booleanProvider(): array
+    {
+        return array(
+            [true, true, true],
+            [true, false, false],
+            [false, true, false],
+            [false, false, true],
+        );
+    }
 }
