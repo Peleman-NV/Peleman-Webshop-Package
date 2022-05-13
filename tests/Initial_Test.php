25ec98869387ec064683fb6db134b83287d1a1f4
KaiH <kai@helsen.info>
2022-05-13 08:53:58 +0200

    removed pointless logging classes

--- tests/Initial_Test.php
+++ tests/Initial_Test.php
@@ -6,27 +6,14 @@ namespace PWP\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-class InitialTest extends TestCase
+class Initial_Test extends TestCase
 {
-    public function testBasicTestingOperation(): void
+    public function test_basic_testing_operation(): void
     {
         $this->assertTrue(true, "most basic test failed., true is not asserted as true.");
     }
 
-    /**
-     * Undocumented function
-     * @dataProvider booleanProvider
-     * @param boolean $a
-     * @param boolean $b
-     * @param boolean $expected
-     * @return void
-     */
-    public function testDataProvision(bool $a, bool $b, bool $expected): void
-    {
-        $this->assertEquals($expected, ($a === $b), "error with data: {$a} equals {$b} should equivalate to {$expected}");
-    }
-
-    public function booleanProvider(): array
+    public function boolean_provider(): array
     {
         return array(
             [true, true, true],
@@ -34,5 +21,18 @@ class InitialTest extends TestCase
             [false, true, false],
             [false, false, true],
         );
+    }
+
+    /**
+     * Undocumented function
+     * @dataProvider boolean_provider
+     * @param boolean $a
+     * @param boolean $b
+     * @param boolean $expected
+     * @return void
+     */
+    public function test_data_provision(bool $a, bool $b, bool $expected): void
+    {
+        $this->assertEquals($expected, ($a === $b), "error with data: {$a} equals {$b} should equivalate to {$expected}");
     }
 }
---
+++ tests/readme.md
@@ -0,0 +1,7 @@
+# Tests
+
+to run tests, open bash in plugin root folder and run ```./vendor/bin/phpunit```
+
+DO NOT PUSH TO GIT IF ANY TESTS FAIL
+
+I'll see about automating this in due time
