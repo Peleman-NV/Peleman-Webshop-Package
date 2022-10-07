df33bcfaa73e967b79d8b0498c8a49714a1ff8c5
KaiH <kai@helsen.info>
2022-10-07 10:02:20 +0200

    0.3.28 patch

--- PelemanWebshopPackage.php
+++ PelemanWebshopPackage.php
@@ -22,7 +22,7 @@ use PWP\includes\versionControl\PWP_VersionController;
  * requires PHP:      7.4
  * requires at least: 5.9.0
  * Description:       In-development umbrella project of the Peleman Product Uploader and Print Partner Integrator.
- * Version:           0.3.26
+ * Version:           0.3.28
  * Author:            Kai Helsen
  * Author URI:        https://github.com/KaiHelsen
  * License:           GPL-2.0+
@@ -34,7 +34,7 @@ use PWP\includes\versionControl\PWP_VersionController;
 defined('WPINC') || die;
 
 //define PWP constant values
-define('PWP_VERSION', '0.3.26');
+define('PWP_VERSION', '0.3.28');
 !defined('PWP_OPTION_GROUP')    ? define('PWP_OPTION_GROUP', 'PWP_OPTIONS') : null;
 !defined('PWP_TEXT_DOMAIN')     ? define('PWP_TEXT_DOMAIN', 'PelemanWebshopPackage') : null;
 !defined('PWP_THUMBNAIL_DIR')   ? define('PWP_THUMBNAIL_DIR', WP_CONTENT_DIR . '/uploads/pwp/thumbnails') : null;
