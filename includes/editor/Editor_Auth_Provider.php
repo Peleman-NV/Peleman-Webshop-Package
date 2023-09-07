0225e32a9a6e9cff89210aaf8044acd3e6bec299
KaiHelsen <kai@helsen.info>
2023-09-07 10:27:52 +0200

    implementing authorization rework

---
+++ includes/editor/Editor_Auth_Provider.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PWP\includes\editor;
+
+use Firebase\JWT\JWT;
+
+class Editor_Auth_Provider
+{
+    private string $api_key;
+    private string $customer_id;
+    private string $domain;
+
+    public function __construct()
+    {
+        $this->api_key      = get_option('pie_api_key', 'https://deveditor.peleman.com');
+        $this->customer_id  = get_option('pie_customer_id', '');
+        $this->domain       = get_option('pie_domain', '');
+    }
+
+    public function get_domain(): string
+    {
+        return $this->domain;
+    }
+
+    public function get_api_key(): string
+    {
+        return $this->api_key;
+    }
+
+    public function get_customer_id(): string
+    {
+        return $this->customer_id;
+    }
+
+    public function get_signed_url_token(): array
+    {
+        $payload = [
+            'iss'       => get_permalink(),
+            'aud'       => $this->domain,
+            'sub'       => $this->customer_id,
+            'iat'       => current_time('timestamp', true),
+        ];
+
+        $jwt = JWT::encode($payload, $this->api_key, 'HS256');
+        return ['signature' => $jwt];
+    }
+
+    public function get_auth_header(): array
+    {
+        $header = ['PIEAPIKEY' => $this->api_key];
+
+        return $header;
+    }
+}
